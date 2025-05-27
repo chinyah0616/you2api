cc18a5f91cd03ec1654600693528dd4bf882506c
chinyah0616 <39376127+chinyah0616@users.noreply.github.com>
2025-05-28 07:18:00 +0800

    Update main.go

--- api/main.go
+++ api/main.go
@@ -161,6 +161,8 @@ var modelMap = map[string]string{
 	"command-r-plus":          "command_r_plus",
 	"claude-3-7-sonnet":       "claude_3_7_sonnet",
 	"claude-3-7-sonnet-think": "claude_3_7_sonnet_thinking",
+	"claude-4-sonnet":       "claude_4_sonnet",
+	"claude-4-sonnet-think": "claude_4_sonnet_thinking",
 	"gpt-4.5-preview":         "gpt_4_5_preview",
 	"gemini-2.5-pro":	   "gemini_2_5_pro_preview",
 	"o3":	   "openai_o3",
